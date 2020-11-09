324184cfee1fd793ce2623373d6bac5c43d0a717
Shrey Chandra <shrey.2012.chandra@gmail.com>
2020-11-10 02:02:47 +1030

    Changed icons to darksky & custom BG for all weather conditions

--- app.js
+++ app.js
@@ -1,1 +1,101 @@
-var text = document.getElementById();
+window.addEventListener('load', ()=> {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let weatherIcon = document.querySelector('icon');
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(pos => {  
+            long = 138.5406;//pos.coords.longitude;
+            lat = -34.8864;//pos.coords.latitude;
+        
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=47f166773e351368285402b79068ea73&units=metric`;
+            console.log(api);
+
+        
+            fetch(api)
+                .then(response => response.json())   
+                .then(data => {
+                    console.log(data);
+                    var temp = data['main']['temp'];
+                    var tempdes = data['weather'][0]['description'];
+                    tempdes = "mist";
+                    var country = data['sys']['country'];
+                    var area = data['name'];
+                    var iconid = data['weather'][0]['icon'];
+                    var newicon = 'http://openweathermap.org/img/wn/'+iconid+'@2x.png';
+
+                //Set DOM elements
+                    temperatureDegree.textContent = temp;
+                    temperatureDescription.textContent = tempdes;
+                    locationTimezone.textContent = country + " - " + area;
+                    // ReplacingImage(newicon);
+
+                    //alternate icons
+                    var icon;
+                    const hours = new Date().getHours();
+                    const isDayTime = hours > 6 && hours < 20;
+                    console.log(isDayTime);
+                    if(tempdes == "clear sky" && isDayTime ){//&& time < 01:14:22 ){
+                        icon = "CLEAR_DAY";
+                        document.body.style.background = "linear-gradient(rgb(236, 219, 63),rgb(214, 108, 21))";
+                    }else if((tempdes == "clear sky" && !isDayTime )){
+                        icon = "CLEAR_NIGHT";
+                        document.body.style.background = "linear-gradient(rgb(63, 80, 236),rgb(14, 34, 145)) ";
+                    }else if(tempdes == "few clouds" && isDayTime ){
+                        icon = "PARTLY_CLOUDY_DAY";
+                    }else if((tempdes == "few clouds" && !isDayTime )){
+                        icon = "PARTLY_CLOUDY_NIGHT";
+                    }else if(tempdes == "scattered clouds"){
+                        icon = "CLOUDY";
+                    }else if(tempdes == "shower rain" || tempdes == "rain"){
+                        icon = "RAIN";
+                    }else if(tempdes == "thunderstorm"){
+                        icon = "WIND";
+                        document.body.style.background = "grey";
+                    }else if(tempdes == "snow" ){
+                        icon = "SNOW"
+                        document.body.style.color ="black";
+                        document.body.style.background = "white";
+                    }else if(tempdes == "mist"){
+                        icon = "FOG";
+                        document.body.style.background = "rgb(194, 204, 120)";
+                    }
+                    // var icon = "PARTLY_CLOUDY_DAY";
+                    setIcons(icon, document.querySelector(".icon"));
+                    
+                    
+                });
+
+        });
+
+        function setIcons(icon, iconIDsky) {
+            if(icon == "SNOW")
+                var skycons = new Skycons({color:"black"});
+            else
+                var skycons = new Skycons({color:"white"});
+            skycons.play();
+            return skycons.set(iconIDsky,Skycons[icon]);
+        }
+
+        
+    }
+
+    
+
+    // function ReplacingImage(newUrl){ 
+ 
+    //     document.getElementById("x").src=newUrl;
+     
+    // } 
+    // else{
+    //     h1.textContext = "Permission required";
+    // }
+
+    // function setIcons(icon,iconID){
+    //     const skycons = new skycons({color:"white"});
+    //     const  
+    // }
+});
